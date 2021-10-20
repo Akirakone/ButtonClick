2d487a5eeaf758f79dc917336e146b5372fc49ba
Akirakone <ca646@khels.org>
2021-10-19 22:44:27 -0400

    buttonclick

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function logout(element) {
+    element.innerText = "logout";
+}
+
+function linuxAlert() {
+    alert("Ninja was liked");
+}
+
+function hide(element) {
+    element.remove();
+}
